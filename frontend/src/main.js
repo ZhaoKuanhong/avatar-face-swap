655d9d34e34e2f775af8d1bf1d3cefb2a91a57e8
faspand <me@faspand.top>
2025-05-22 13:39:18 +0800

    bug fix

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -37,7 +37,7 @@ const PublicRoutes = [
                 },
             },
             {
-                path: 'list/:event_id/edit',
+                path: 'list/:event_id/export',
                 name: 'EventPicExport',
                 component: () => import('@/components/PicEdit.vue')
             },
